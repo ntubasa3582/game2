a7cae82363c2d826584b2ecc365ac7c3b35ea8f9
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2023-06-12 18:01:46 +0900

    stage4作成

--- Assets/script/move/scale2.cs
+++ Assets/script/move/scale2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scale : MonoBehaviour
+public class scale2 : MonoBehaviour
 {
     int _rox;
     int _roy;
@@ -16,18 +16,18 @@ public class scale : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) 
-        {
-            transform.Rotate(0f, 0f, 45f);
-        }
-        if(Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
             transform.Rotate(0f, 0f, -45f);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.Rotate(0f, 0f, 45f);
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
 
         }
